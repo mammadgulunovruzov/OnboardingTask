8d982e9e7c5bccb3bbdff3389de71d14c2b89ae1
Novruzov Mammadgulu <a12145128@unet.univie.ac.at>
2025-02-19 17:57:10 +0100

    Initial Commit

---
+++ OnboardingTaskTests/OnboardingTaskTests.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingTaskTests.swift
+//  OnboardingTaskTests
+//
+//  Created by Mammadgulu Novruzov on 19.02.25.
+//
+
+import Testing
+@testable import OnboardingTask
+
+struct OnboardingTaskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
